35c9dc4f1411457ad9d1df681269b002ea76cc5a
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-17 17:13:14 -0500

    react-router-working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import TopBar from './TopBar';
 import './css/App.css';
 import Lunch from './Lunch';
@@ -6,12 +7,13 @@ import Lunch from './Lunch';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TopBar />
-        <Lunch/>
-      </div>
+      <Router className="App">
+        <div>
+          <TopBar />
+          <Route exact path="/" component={Lunch} />
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
