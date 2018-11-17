f5613845d9ea3ce4190708c6eca87e7436e625db
alewicki <contact@andre-developer.com>
2018-11-17 18:24:09 -0500

    Add lunch scanning view

--- src/LunchScanning.js
+++ src/LunchScanning.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 import './css/LunchScanning.css';
+import LunchBag from './img/lunchbag.svg';
 
 class LunchScanning extends Component {
+
 
   render() {
     return (
       <div className="lunch-scanning">
-        <h2>Choose time</h2>
-
+        <h2>Looking for your lunch mates...</h2>
+        <img src={LunchBag} className="bag" />
       </div>
     );
   }
