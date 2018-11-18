a94f085e7a38ba30add40e4f4191cabeabbd2950
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-17 23:19:12 -0500

    sick absolute positioning css.

--- src/TopBar.js
+++ src/TopBar.js
@@ -5,11 +5,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import lunchpools from './img/lunchpools.svg';
-import './css/TopBar.css'
+import './css/TopBar.css';
+import brian from './img/brian.png';
 
 const styles = {
   root: {
-    flexGrow: '1'
+    flexGrow: '1',
   },
 };
 
@@ -23,6 +24,18 @@ function SimpleAppBar(props) {
           <div className="logo-wrapper">
             <img src={lunchpools} />
           </div>
+
+          <img
+            src={brian}
+            style={{
+              position: 'absolute',
+              top: '0',
+              right: '10px',
+              height: '100%',
+              padding: '7px',
+              boxSizing: 'border-box',
+            }}
+          />
         </Toolbar>
       </AppBar>
     </div>
