3acb87e22ec75257ad100432900804cd7fc36aeb
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-19 15:47:33 -0500

    fomatting change

--- src/LunchResults.js
+++ src/LunchResults.js
@@ -18,9 +18,9 @@ class LunchResults extends Component {
   constructor() {
     super();
     this.names = [
-      {name: 'Betty P.', photoSrc: betty},
-      {name: 'Richard M.', photoSrc: richard},
-      {name: 'Alin I.', photoSrc: alin},
+      { name: 'Betty P.', photoSrc: betty },
+      { name: 'Richard M.', photoSrc: richard },
+      { name: 'Alin I.', photoSrc: alin },
     ];
   }
 
@@ -28,23 +28,23 @@ class LunchResults extends Component {
     return (
       <div>
         <TopBar />
-      <div className="lunch-result">
-        <h2>We did it! We found your lunch mates. </h2>
-        <h2>12:00, Fabrica Pizza 142 S Meridian Ave, Tampa, FL 33602.</h2>
-        <img className="selectedMap" src={selectedMap} alt="selectedMap" />
-        <List dense>
-          {[0, 1, 2].map(value => (
-            <ListItem key={value} button>
-              <Avatar alt="Remy Sharp" src={this.names[value].photoSrc} />
-              <ListItemText primary={this.names[value].name} />
-              <ListItemSecondaryAction>
-                <IconButton aria-label="Comments">
-                  <CommentIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
+        <div className="lunch-result">
+          <h2>We did it! We found your lunch mates. </h2>
+          <h2>12:00, Fabrica Pizza 142 S Meridian Ave, Tampa, FL 33602.</h2>
+          <img className="selectedMap" src={selectedMap} alt="selectedMap" />
+          <List dense>
+            {[0, 1, 2].map(value => (
+              <ListItem key={value} button>
+                <Avatar alt="Remy Sharp" src={this.names[value].photoSrc} />
+                <ListItemText primary={this.names[value].name} />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Comments">
+                    <CommentIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
         </div>
       </div>
     );
